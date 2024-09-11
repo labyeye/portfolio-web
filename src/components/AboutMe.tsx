0c1b067196447f0411a635bafa171e678fe7e837
Labh Bothra <labhbothra1380@gmail.com>
2024-09-11 23:47:21 +0530

    Updated Profile Header

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,36 +1,48 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 import heroImage from "@/assets/Ahmed.jpeg";
-import { Twitter, GithubIcon, Linkedin } from 'lucide-react';
+import { Twitter, GithubIcon, Linkedin } from "lucide-react";
 
 const AboutMe: React.FC = () => {
   return (
-    <div className="bg-white dark:bg-gray-800 rounded-xl shadow-lg overflow-hidden transition-all duration-300 hover:shadow-xl md:col-span-3 lg:col-span-4">
+    <div className="overflow-hidden rounded-xl bg-white shadow-lg transition-all duration-300 hover:shadow-xl dark:bg-gray-800 md:col-span-3 lg:col-span-4">
       <div className="p-6">
-        <div className="flex flex-col sm:flex-row items-center sm:items-start space-y-4 sm:space-y-0 sm:space-x-6">
+        <div className="flex flex-col items-center space-y-4 sm:flex-row sm:items-start sm:space-x-6 sm:space-y-0">
           <Image
             src={heroImage}
             alt="Ahmed's profile picture"
-            className="rounded-full shadow-md h-32 w-32 object-cover object-center ring-4 ring-blue-500 dark:ring-blue-400"
+            className="h-32 w-32 rounded-full object-cover object-center shadow-md ring-4 ring-blue-500 dark:ring-blue-400"
           />
           <div className="flex-1 text-center sm:text-left">
-            <h1 className="text-2xl font-bold text-gray-900 dark:text-white mb-1">Labh Chandra Bothra</h1>
-            <p className="text-blue-600 dark:text-blue-400 mb-4">@medevs</p>
-            <p className="text-gray-700 dark:text-gray-300 mb-4 leading-relaxed">
-              Hey there, I&apos;m Ahmed, a Software Developer based in Bremen, Germany. I dive deep into JavaScript and love crafting sleek, efficient web apps. With an eye for detail and a hunger for learning, I&apos;m all about delivering high-quality solutions that meet both user needs and business objectives.
+            <h1 className="mb-1 text-2xl font-bold text-gray-900 dark:text-white">
+              Labh Chandra Bothra
+            </h1>
+            <p className="mb-4 text-blue-600 dark:text-blue-400">@labyeye</p>
+            <p className="mb-4 leading-relaxed text-gray-700 dark:text-gray-300">
+              Hey there, I&apos;m Labh, Skilled in JavaScript and web
+              development, I craft efficient, user-focused web applications. In
+              addition to web development, I have a passion for game
+              development, where I apply my skills to create engaging and
+              immersive experiences. With a commitment to high-quality solutions
+              and a drive for continuous learning, I align technology with
+              business goals and enhance user experiences in both web and game
+              projects.
             </p>
-            <p className="text-gray-700 dark:text-gray-300 mb-4 leading-relaxed">
-              AI? Oh, that&apos;s my jam. I geek out on building software that taps into AI magic.
-            </p>
-            <div className="flex justify-center sm:justify-start space-x-4">
-              <a href="https://twitter.com/ahmedoublihi" target='_blank' className="text-blue-400 hover:text-blue-500 transition-colors duration-300">
-                <Twitter className="w-6 h-6" />
+
+            <div className="flex justify-center space-x-4 sm:justify-start">
+              <a
+                href="https://github.com/labyeye"
+                target="_blank"
+                className="text-gray-700 transition-colors duration-300 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white"
+              >
+                <GithubIcon className="h-6 w-6" />
               </a>
-              <a href="https://github.com/medevs" target='_blank' className="text-gray-700 dark:text-gray-300 hover:text-gray-900 dark:hover:text-white transition-colors duration-300">
-                <GithubIcon className="w-6 h-6" />
-              </a>
-              <a href="https://linkedin.com/in/ahmed-oublihi" target='_blank' className="text-blue-700 hover:text-blue-800 transition-colors duration-300">
-                <Linkedin className="w-6 h-6" />
+              <a
+                href="https://www.linkedin.com/in/labh-bothra/"
+                target="_blank"
+                className="text-blue-700 transition-colors duration-300 hover:text-blue-800"
+              >
+                <Linkedin className="h-6 w-6" />
               </a>
             </div>
           </div>
@@ -38,6 +50,6 @@ const AboutMe: React.FC = () => {
       </div>
     </div>
   );
-}
+};
 
 export default AboutMe;
